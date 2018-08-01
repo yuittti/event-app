eb2dd8cda93f852536c77ce5b01580360afe5b49
yuittti <kozachenkos@gmail.com>
2018-08-01 18:37:36 +0300

    Add Marker

--- src/screens/events/EventMap.js
+++ src/screens/events/EventMap.js
@@ -1,16 +1,49 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
-import { MapView } from 'expo';
+import { View, StyleSheet, Text } from 'react-native';
+import { MapView, Permissions, Location } from 'expo';
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
 
+@observer
 class EventMapScreen extends Component {
 
     static navigationOptions = {
         title: 'map'
     }
+
+    @observable errorMessage = null;
+    @observable permissionAsked = false;
+    @observable location = null;
+
+    async componentDidMount() {
+        const {status} = await Permissions.askAsync(Permissions.LOCATION);
+        this.permissionAsked = true;
+        if (status !== 'granted') this.errorMessage = 'permission denied';
+        this.location = await Location.getCurrentPositionAsync();
+    }
     
     render() { 
+        if (!this.permissionAsked) return <Text>No permission yet</Text>
+        if (this.errorMessage) return <Text>{this.errorMessage}</Text>
+
+        const region = this.location && {
+            ...this.location.coords,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+        };
+
+        const marker = this.location && <MapView.Marker
+                coordinate={this.location.coords}
+                title='event place'
+            />
+
         return (
-            <MapView style={styles.map} />
+            <MapView 
+                style={styles.map} 
+                region = {region}
+            >
+                {marker}
+            </MapView>
         );
     }
 }
@@ -19,6 +52,6 @@ const styles = StyleSheet.create({
     map: {
         flex: 1
     }
-})
+});
  
 export default EventMapScreen;
